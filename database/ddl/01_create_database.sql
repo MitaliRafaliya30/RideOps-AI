3ad4c5c4a2d4b331a318bcedc6b5c881920a88e2
MitaliRafaliya30 <mitalirafaliya@gmail.com>
2026-08-06 18:47:25 +0530

    chore: initialize RideOps AI project

---
+++ database/ddl/01_create_database.sql
@@ -0,0 +1,8 @@
+-- ==========================================================
+-- Project : RideOps AI
+-- Module  : PostgreSQL Master Data
+-- File    : 01_create_database.sql
+-- Purpose : Create PostgreSQL database
+-- ==========================================================
+
+CREATE DATABASE rideops_ai_db;
---
+++ database/ddl/02_create_schema.sql
@@ -0,0 +1,8 @@
+-- ==========================================================
+-- Project : RideOps AI
+-- Module  : PostgreSQL Master Data
+-- File    : 02_create_schema.sql
+-- Purpose : Create Master Schema
+-- ==========================================================
+
+CREATE SCHEMA IF NOT EXISTS master;
